6b33c8bfac38144d9f2cac54b5ebffe772a1f6db
Kyle Carey <135062060+kylecarey2@users.noreply.github.com>
2024-12-04 18:31:34 -0500

    Basic Property class

---
+++ property.cc
@@ -0,0 +1,125 @@
+#include "property.h"
+
+#include <cstdlib>
+
+Property::Property() {
+    id = -1;
+    price = -1;
+    name = "-";
+    color = "-";
+    ownerId = -1;
+}  /// Property
+
+Property::Property(int newId) {
+    if (newId > -1) {
+        id = newId;
+    }
+}  /// Property
+
+Property::Property(string propertyData) {
+    // id|name|price|color
+    /// DOES NOT ERROR CHECK
+    int startIndex = 0, endIndex = 0, separatorCount = 0;
+    string temp;
+    for (size_t i = 0; i < propertyData.length(); i++) {
+        char c = propertyData.at(i);
+        bool alnumSeen = false;
+        if (c == '|') {
+            /// Assign temp to respective variable based on separatorCount
+            if (separatorCount == 0) {
+                id = stoi(temp);
+            }
+            else if (separatorCount == 1) {
+                name = temp;
+            }
+            else if (separatorCount == 2) {
+                price = stoi(temp);
+            }
+
+            /// increment separatorCount
+            separatorCount++;
+        }
+        else if (isalnum(c) || c == '-') {
+            /// Sets starting index of substring and ending index of substring
+            if (!alnumSeen) {
+                startIndex = i;
+                alnumSeen = true;
+            }
+            else {
+                endIndex = i;
+            }
+        }
+
+        temp = propertyData.substr(startIndex, endIndex - startIndex + 1);
+    }
+
+    if (separatorCount == 3) {
+        color = temp;
+    }
+
+    ownerId = -1;
+}  /// Property
+
+int Property::getId() const {
+    return id;
+}  /// getId
+
+int Property::getPrice() const {
+    return price;
+}  /// getPrice
+
+string Property::getName() const {
+    return name;
+}  /// getName
+
+string Property::getColor() const {
+    return color;
+}  /// getColor
+
+int Property::getOwnerId() const {
+    return ownerId;
+}  /// getOwnerId
+
+void Property::setId(int newId) {
+    if (newId > -1) {
+        id = newId;
+    }
+}  /// setId
+
+void Property::setPrice(int newPrice) {
+    if (newPrice > -1) {
+        price = newPrice;
+    }
+}  /// setPrice
+
+void Property::setName(string newName) {
+    if (!newName.empty()) {
+        name = newName;
+    }
+}  /// setName
+
+void Property::setColor(string newColor) {
+    if (!newColor.empty()) {
+        color = newColor;
+    }
+}  /// setColor
+
+void Property::setOwnerId(int newOwnerId) {
+    if (newOwnerId > -1 && ownerId == -1) {
+        ownerId = newOwnerId;
+    }
+}  /// setOwnerId
+
+void Property::removeOwnerId() {
+    ownerId = -1;
+}  /// removeOwnerId
+
+bool verifyUniqueId(const int &newId, const vector<Property> properties) {
+    for (size_t i = 0; i < properties.size(); i++) {
+        if (newId == properties[i].getId()) {
+            return false;
+        }
+    }
+
+    return true;
+}  /// verifyUniqueId
---
+++ property.h
@@ -0,0 +1,142 @@
+#ifndef PROPERTY_H
+#define PROPERTY_H
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+class Property {
+ public:
+    /**
+     * Function:    Property
+     *              sets to default values for each property
+     *
+     */
+    Property();
+
+    /**
+     * Function:    Property
+     *              sets the id of the Property with specified value
+     *
+     * @param newId - id, as an int
+     */
+    Property(int newId);
+
+    /**
+     * Function:    Property
+     *              sets all of the member data if Property data is read from a file
+     *
+     * @param propertyData - string of property data in id|name|price|color format
+     */
+    Property(string propertyData);
+
+    /**
+     * Function:    getId
+     *              returns the Property's id
+     *
+     * @return - id, as an int
+     */
+    int getId() const;
+
+    /**
+     * Function:    getPrice
+     *              returns the Property's price
+     *
+     * @param paramName - paramDescription
+     * @return - price, as an int
+     */
+    int getPrice() const;
+
+    /**
+     * Function:    getName
+     *              returns the Property's name
+     *
+     * @return - name, as a string
+     */
+    string getName() const;
+
+    /**
+     * Function:    getColor
+     *              returns the Property's color
+     *
+     * @return - color, as a string
+     */
+    string getColor() const;
+
+    /**
+     * Function:    getOwnerId
+     *              returns the Property's current ownerId
+     *
+     * @return - ownerId, as an int
+     */
+    int getOwnerId() const;
+
+    /**
+     * Function:    setId
+     *              sets the Property id to a value above -1
+     *
+     * @param newId - the id to set to, as an int
+     * @return - void
+     */
+    void setId(int newId);
+
+    /**
+     * Function:    setPrice
+     *              sets the Property price to a value above -1
+     *
+     * @param newPrice - the price to set to, as an int
+     * @return - void
+     */
+    void setPrice(int newPrice);
+
+    /**
+     * Function:    setName
+     *              sets the Property name to a value above that is not empty
+     *
+     * @param newName - the name to set to, as an string
+     * @return - void
+     */
+    void setName(string newName);
+
+    /**
+     * Function:    setColor
+     *              sets the Property color to a value above that is not empty
+     *
+     * @param newColor - the color to set to, as an string
+     * @return - void
+     */
+    void setColor(string newColor);
+
+    /**
+     * Function:    setOwnerId
+     *              sets the Property ownerId to a value above that is not empty
+     *
+     * @param newOwnerId - the ownerId to set to, as an int
+     * @return - void
+     */
+    void setOwnerId(int newOwnerId);
+
+    /**
+     * Function:    removeOwnerId
+     *              remove's the Property's current ownerId resetting it to -1
+     *
+     * @return - void
+     */
+    void removeOwnerId();
+
+    /**
+     * Function:    verifyUniqueId
+     *              returns if the id has already been used in other properties
+     *
+     * @param newId - the id, as an int
+     * @return - bool, true if good to use, false if not
+     */
+    friend bool verifyUniqueId(const int &newId, const vector<Property> properties);
+
+ private:
+    int id;        /// id of property
+    int price;     /// price of property
+    string name;   /// name of property
+    string color;  /// color of property
+    int ownerId;   /// ownerId of property
+};
+#endif
