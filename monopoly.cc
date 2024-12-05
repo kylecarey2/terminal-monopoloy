150990027afa463fede81b99a4bf9d2b4a7b6c28
Kyle Carey <135062060+kylecarey2@users.noreply.github.com>
2024-12-05 17:20:38 -0500

    Added createPlayers function

--- monopoly.cc
+++ monopoly.cc
@@ -11,6 +11,7 @@
 #include <fstream>
 #include "die.h"
 #include "property.h"
+#include "player.h"
 using namespace std;
 
 /// function prototypes
@@ -22,21 +23,44 @@ using namespace std;
  * @return - bool, true if duplicate, false if not
  */
 bool duplicateId(const int &newId, const vector<Property> properties);
-void loadProperties(vector<Property> &properties);
+void loadProperties(vector<Property> &properties); // loads the properties from file into a vector of properties
+void createPlayers(vector<Player> &players); // prompts user for player number then their names
 
 
 const string FILENAME = "properties.txt";
+const int MAX_PLAYERS = 8;
+const int MAX_NAME_LENGTH = 15; // play with this number to find ideal length
 
 int main(int argc, char const *argv[]) {
+    // Player one;
+    // Player two(3);
+    // Player three(2, "Test");
+    // cout << one.getId() << " " << one.getName() << " " << one.getBalance() << endl;
+    // cout << two.getId() << " " << two.getName() << " " << two.getBalance() << endl;
+    // cout << three.getId() << " " << three.getName() << " " << three.getBalance() << endl;
+    // three.addBalance(200);
+    // cout << three.getId() << " " << three.getName() << " " << three.getBalance() << endl;
 
-    vector<Property> properties;
-    loadProperties(properties);
-    for (size_t i = 0; i < properties.size(); i++) {
-        cout << properties[i].getId() << "\t";
-        cout << properties[i].getName() << "\t";
-        cout << properties[i].getPrice() << "\t";
-        cout << properties[i].getColor() << endl;
+
+    
+    // vector<Property> properties;
+    // loadProperties(properties);
+    // for (size_t i = 0; i < properties.size(); i++) {
+    //     cout << properties[i].getId() << "\t";
+    //     cout << properties[i].getName() << "\t";
+    //     cout << properties[i].getPrice() << "\t";
+    //     cout << properties[i].getColor() << endl;
+    // }
+
+    vector<Player> players;
+    createPlayers(players);
+    for (size_t i = 0; i < players.size(); i++) {
+        cout << players[i].getId() << "\t";
+        cout << players[i].getName() << "\t";
+        cout << players[i].getBalance() << "\n";
     }
+
+
     return 0;
 }/// main
 
@@ -80,4 +104,39 @@ void loadProperties(vector<Property> &properties) {
     }
 
     in.close();
+}
+
+void createPlayers(vector<Player> &players) {
+    /// Prompt user for number of players
+    int numPlayers;
+    cout << "Enter the number of players (2-" << MAX_PLAYERS << "): ";
+    cin >> numPlayers;
+    if (cin.fail() || numPlayers < 2 || numPlayers > MAX_PLAYERS) {
+        cout << "Error: enter an integer between 2-" << MAX_PLAYERS << endl;
+        exit(0);
+    } 
+
+    /// Create new players
+    cout << "Names must be below " << MAX_NAME_LENGTH << " characters" << endl;
+    for (int i = 0; i < numPlayers; i++) {
+        /// Prompt for player's name
+        string name;
+        cout << "                                   ";
+        for (int i = 0; i < MAX_NAME_LENGTH; i++) { cout << "_"; }
+        cout << "\r";
+        cout << "Please enter the name of player " << i + 1 << ": ";
+        
+        /// Read in and verify name
+        cin >> name;
+        if (cin.fail() || name.length() > MAX_NAME_LENGTH) {
+            cout << "Error: enter a string between 1-" << MAX_NAME_LENGTH << " characters" << endl;
+            exit(0);
+        }
+
+        /// Create Player and add to vector
+        Player p(i, name);
+        players.push_back(p);
+    }
+
+    cout << endl;
 }
