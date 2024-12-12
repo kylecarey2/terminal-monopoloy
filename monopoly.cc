62a47cb19beda1f6f09de27e1ffbdd205a2734b9
Kyle Carey <135062060+kylecarey2@users.noreply.github.com>
2024-12-11 20:33:41 -0500

    Added quit, confirm, help, cycle functions

--- monopoly.cc
+++ monopoly.cc
@@ -9,9 +9,11 @@
 #include <iomanip>
 #include <cstdlib>
 #include <fstream>
+#include <cctype>
 #include "die.h"
 #include "property.h"
 #include "player.h"
+#include "board.h"
 using namespace std;
 
 /// function prototypes
@@ -25,42 +27,35 @@ using namespace std;
 bool duplicateId(const int &newId, const vector<Property> properties);
 void loadProperties(vector<Property> &properties); // loads the properties from file into a vector of properties
 void createPlayers(vector<Player> &players); // prompts user for player number then their names
+void confirmPlayers(const vector<Player> &players); // prompts user to confirm players playing the game
+void cyclePlayers(vector<Player> &players); // cycles the players
+void cyclePlayers(vector<Player> &players, const int &randNum); // cycles the players a specified amount of times
+void quitGame(); // quits the game
+void showActions(); // shows the actions
 
 
-const string FILENAME = "properties.txt";
-const int MAX_PLAYERS = 8;
+
+const string FILENAME = "prop.txt";
+const int MAX_PLAYERS = 6;
 const int MAX_NAME_LENGTH = 15; // play with this number to find ideal length
 
 int main(int argc, char const *argv[]) {
-    // Player one;
-    // Player two(3);
-    // Player three(2, "Test");
-    // cout << one.getId() << " " << one.getName() << " " << one.getBalance() << endl;
-    // cout << two.getId() << " " << two.getName() << " " << two.getBalance() << endl;
-    // cout << three.getId() << " " << three.getName() << " " << three.getBalance() << endl;
-    // three.addBalance(200);
-    // cout << three.getId() << " " << three.getName() << " " << three.getBalance() << endl;
-
-
-    
-    // vector<Property> properties;
-    // loadProperties(properties);
-    // for (size_t i = 0; i < properties.size(); i++) {
-    //     cout << properties[i].getId() << "\t";
-    //     cout << properties[i].getName() << "\t";
-    //     cout << properties[i].getPrice() << "\t";
-    //     cout << properties[i].getColor() << endl;
-    // }
-
+    /// Create variables / objects associated with the game
+    vector<Property> properties;
     vector<Player> players;
+    Die D1, D2;
+
+    /// Show welcome message with instructions
+
+    /// Load properties and players into vectors and create board
+    loadProperties(properties);
     createPlayers(players);
-    for (size_t i = 0; i < players.size(); i++) {
-        cout << players[i].getId() << "\t";
-        cout << players[i].getName() << "\t";
-        cout << players[i].getBalance() << "\n";
-    }
+    confirmPlayers(players);
+    Board board(&properties, &players);
+    cout << "Starting game..." << endl;
 
 
+    cout << endl;
     return 0;
 }/// main
 
@@ -97,9 +92,9 @@ void loadProperties(vector<Property> &properties) {
         }
     }
 
-    /// Make sure there's 28 properties needed to fill the board
-    if (properties.size() != 28) {
-        cout << "Error: \"" << FILENAME << "\" must have 27 properties" << endl;
+    // Make sure there's 40 properties needed to fill the board
+    if (properties.size() != 40) {
+        cout << "Error: \"" << FILENAME << "\" must have 40 properties" << endl;
         exit(0);
     }
 
@@ -140,3 +135,52 @@ void createPlayers(vector<Player> &players) {
 
     cout << endl;
 }
+
+void confirmPlayers(const vector<Player> &players) {
+    cout << "Confirm the following players are playing:" << endl;
+    for (size_t i = 0; i < players.size(); i++) {
+        cout << " -" << players.at(i).getName() << endl;
+    }
+    cout << endl;
+    cout << "Is this correct? (y/n): ";
+    string response;
+    cin >> response;
+    if (tolower(response.at(0)) != 'y') {
+        cout << "Players not confirmed. Terminating program" << endl;
+        exit(0);
+    }
+}
+
+void quitGame() {
+    string response;
+    do {
+        cout << "All progress will be lost. Confirm you wish to quit the game. (y/n): ";
+        cin >> response;
+
+        if (tolower(response.at(0)) == 'y') {
+            cout << "Quitting the game" << endl;
+            exit(0);
+        }
+        else if (tolower(response.at(0)) == 'n') {
+            cout << "The game will continue" << endl;
+            break;
+        }
+    } while(response.at(0) != 'y' || response.at(0) != 'n');
+}
+
+void showActions() {
+    cout << "Here are the following actions:" << endl;
+    cout << "r - roll the dice, u - upgrade current property, h - show actions, q - quit the game" << endl; // add change identifier and save game once we get there
+}
+
+void cyclePlayers(vector<Player> &players) {
+    players.push_back(players.at(0));
+    players.erase(players.begin());
+}
+
+void cyclePlayers(vector<Player> &players, const int &randNum) {
+    for (int i = 0; i < randNum; i++) {
+        cyclePlayers(players);
+    }
+}
+
